7aaf8fe638db88cbe8edda7a3fea32e29bf30ac8
eneye-cyber <alaoeneye@gmail.com>
2025-10-14 19:41:23 +0100

    manifest fix

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,12 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/js/checkout.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+                'resources/css/checkout.css',
+                'resources/js/checkout.js', // ✅ add this
+            ],
             refresh: true,
         }),
         tailwindcss(),
